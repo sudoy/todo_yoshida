fa215d7f6e50b2d28699733c65392433f1a0542a
吉田 <@TSD-YOSHIDA>
2019-06-27 15:22:02 +0900

    sql修正

--- sql/todo.sql
+++ sql/todo.sql
@@ -1,17 +1,18 @@
 CREATE table todo(
 	id int primary key auto_increment,
 	name varchar(50),
+	detail text,
 	priority varchar(10),
 	timelimit date
 );
 
-insert into todo(id,name,priority,timelimit)
-values ('1','テスト1','★★★','2015-06-20');
+insert into todo(id,name,detail,priority,timelimit)
+values ('1','テスト1','一番優先度が高い。','★★★','2015-06-20');
 
-insert into todo(id,name,priority,timelimit)
-values ('2','テスト2','★★','2015-06-22');
+insert into todo(id,name,detail,priority,timelimit)
+values ('2','テスト2','二番目に優先度が高い','★★','2015-06-22');
 
-insert into todo(id,name,priority,timelimit)
-values ('3','テスト3','★','2015-06-25');
+insert into todo(id,name,detail,priority,timelimit)
+values ('3','テスト3','三番目に優先度が高い','★','2015-06-25');
 
 
